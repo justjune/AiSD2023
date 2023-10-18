5d9ad5e8a398201b0882990c92a6e311e348d2e3
KonstantinPM201 <K.S.Martianov@urfu.me>
2023-10-18 16:19:19 +0500

    Написал операторы сравнения.
    operator>=
    operator<=
    operator>
    operator<
    
    Как починить operator+ не знаю.
    Сейчас появилась идея определять знак суммы с помощью сравнения.

--- Karatsuba algorithm/Karatsuba_algorithm.cpp
+++ Karatsuba algorithm/Karatsuba_algorithm.cpp
@@ -42,7 +42,7 @@ class LongValue{
         
         // Оператор сложения
         LongValue operator+(const LongValue& other) const {
-            size_t maxLength = std::max(length, other.length);
+            size_t maxLength = std::max(length-CountLeadingZeros(*this), other.length-CountLeadingZeros(other));
             
             int* result = new int[maxLength + 1]; // +1 на случай, если сложение вызовет перенос разряда
             std::memset(result, 0, sizeof(int) * (maxLength + 1)); // Инициализация массива результата нулями
@@ -60,15 +60,18 @@ class LongValue{
                 }
                 s=1;
                 
-                if(digitSum<0){
-                    s=-1;
-                    digitSum=-digitSum;
-                }
-                // Вычисление текущей цифры результата и перенос разряда
                
+                // Вычисление текущей цифры результата и перенос разряда
+                //std::cout<<s<<'\n';
+                
                 result[i] = digitSum % 10;
-                result[i + 1] = result[i + 1] + s*(digitSum / 10);
+                result[i + 1] = result[i + 1] + (digitSum / 10);
+
+                std::cout<<result[i]<< ' ';
             }
+            std::cout<<'\n';
+        // Устанавливаем знак результата
+        
             
             // Проверка на ведущий ноль
             size_t resultLength=maxLength+1;
@@ -77,10 +80,11 @@ class LongValue{
                 if(result[i]==0 and i>0){resultLength--;}
                 else{break;}
             }
+            int resultSign = (result[resultLength - 1] == 0) ? 1 : (result[resultLength - 1] < 0 ? -1 : 1);
             
 
             // Создание нового объекта LongValue с результатом сложения
-            LongValue sum(result, resultLength, s);
+            LongValue sum(result, resultLength, resultSign);
 
             // Освобождение выделенной памяти
             delete[] result;
@@ -104,20 +108,20 @@ class LongValue{
         }
         // Оператор сравнения
         bool operator==(const LongValue& other) const {
-            size_t Zeros1=CountLeadingZeros(other);
-            size_t Zeros2=CountLeadingZeros(*this);
-            if (length-Zeros2 != other.length-Zeros1) {
+            size_t l1=length-CountLeadingZeros(*this);
+            size_t l2=other.length-CountLeadingZeros(other);
+            if (l1 != l2) {
                 return false;
             }
             if(sign != other.sign){
-                for (size_t i = 0; i < length-Zeros2; ++i) {
+                for (size_t i = 0; i < l1; ++i) {
                     if ((values[i] != other.values[i]) or values[i]!=0) {
                         return false;
                     }
                 }
             }
             else{
-                for (size_t i = 0; i < length-Zeros2; ++i) {
+                for (size_t i = 0; i < l1; ++i) {
                     if (values[i] != other.values[i]) {
                         return false;
                     }
@@ -125,6 +129,59 @@ class LongValue{
             }
             return true;
         }
+
+        bool operator>(const LongValue& other) const {
+            size_t l1=length-CountLeadingZeros(*this);
+            size_t l2=other.length-CountLeadingZeros(other);
+
+            if (sign!=other.sign) {
+                return sign>other.sign;
+            }
+            else{
+                if(l1 < l2) {
+                    return (sign-1)!=0;
+                }
+                else if(l1 > l2){
+                    return (sign-1)==0;
+                }
+                for (size_t i = l1-1; i>0 ; i--) {
+                    if (values[i]>other.values[i]) {
+                        return true;
+                    }
+                }
+                return false;    
+            }
+        }
+        bool operator<(const LongValue& other) const {
+            size_t l1=length-CountLeadingZeros(*this);
+            size_t l2=other.length-CountLeadingZeros(other);
+
+            if (sign!=other.sign) {
+                return sign<other.sign;
+            }
+            else{
+                if(l1 < l2) {
+                    return (sign-1)==0;
+                }
+                else if(l1 > l2){
+                    return (sign-1)!=0;
+                }
+                for (size_t i = l1-1; i>0 ; i--) {
+                    if (values[i]<other.values[i]) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+
+        bool operator>=(const LongValue& other) const {
+            return !(*(this)<other);
+        }
+        bool operator<=(const LongValue& other) const {
+            return !(*(this)>other);
+        }
+        
 
         int operator[](size_t index) const {
             if (index < length) {
@@ -147,9 +204,6 @@ class LongValue{
 
 
 
-
-
-
 void Test(LongValue& res, LongValue& answer, bool check=true){
     std::cout<< "res: " << res <<", "<< "answer: "<<answer;
     if((res == answer)==check){ 
@@ -160,9 +214,13 @@ void Test(LongValue& res, LongValue& answer, bool check=true){
     }
     std::cout<<'\n';
 }
+
+
 int main() {
+    
     LongValue a1({4,5,3,0,0,0,0},1);
     LongValue la1({4,5,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},1);
+    LongValue nla1({4,5,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},-1);
     LongValue na1({4,5,3},-1);
 
     LongValue b1({4,7,3,3,6},1);
@@ -173,13 +231,31 @@ int main() {
 
     LongValue s1({9,7},1);
     LongValue ns1({9,7},-1);
-    
+
+
+    std::cout << (a1>a1==0)<<'\n';
+    std::cout << (na1>nla1==0)<<'\n';
+    std::cout << (na1<nla1==0)<<'\n';
+    std::cout << (a1>na1==1)<<'\n';
+    std::cout << (a1>s1==1)<<'\n';
+    std::cout << (d1<s1==1)<<'\n';
+    std::cout << (nd1<na1==0)<<'\n';
+    std::cout << '\n';
+
+    std::cout << (a1>=a1==1)<<'\n';
+    std::cout << (na1>=nla1==1)<<'\n';
+    std::cout << (na1<=nla1==1)<<'\n';
+    std::cout << (a1>=na1==1)<<'\n';
+    std::cout << (a1>=s1==1)<<'\n';
+    std::cout << (d1<=s1==1)<<'\n';
+    std::cout << (nd1<=na1==0)<<'\n';
+
     Test(a1,a1,1);
     Test(la1,a1,1);
     Test(ns1,ns1,1);
     Test(a1,na1,0);
     Test(a1,ns1,0);
-    
+
     LongValue res1=a1+a1;
     LongValue answer1({8,0,7},1);
     Test(res1, answer1,1);
@@ -195,5 +271,19 @@ int main() {
     LongValue res4=s1+nb1;
     LongValue answer4({5,9,2,3,6},-1);
     Test(res4, answer4,1);
+
+    LongValue res7=nb1+nb1;
+    LongValue answer7({8,4,7,6,2,1},-1);
+    Test(res7, answer7,1);
+/*
+    LongValue res5=nb1+d1;
+    LongValue answer5({0,7,3,3,6},-1);
+    Test(res5, answer5,1);
+
+    LongValue res6=nb1+nla1;
+    LongValue answer6({8,2,7,3,6},-1);
+    Test(res6, answer6,1);
+*/
+    
     return 0;
 }
